99334b9a379fce31bdf11642b3ebf235b22a5147
emirk <emir.karabey1@hotmail.com>
2022-02-22 14:18:08 +0300

    Application Interface and Model and navigation graph added

---
+++ app/src/main/java/com/example/besinlerkitabi/model/Besin.kt
@@ -0,0 +1,11 @@
+package com.example.besinlerkitabi.model
+
+data class Besin (
+    val besinIsim:String?,
+    val besinKalori:String?,
+    val besinKarbonhidrat:String?,
+    val besinProtein:String?,
+    val besinYag:String?,
+    val besinGorsel:String?
+        ){
+}
